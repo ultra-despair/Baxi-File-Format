25b08197cf73d3d224489c2b589e36ad3d0ce0ea
ultra-despair <despair-ultra@proton.me>
2026-06-03 20:02:28 -0300

    New tables

--- main.lua
+++ main.lua
@@ -16,23 +16,44 @@ for i = 1, #list do
     for temporaryFiles in list:inlines() do
         if string.match(temporaryFiles, "file" .. tostring(i) .. ".baxi$") and i == 1 then
             --table.insert(tableFilled, i)
-            if i == 1 and temporaryFiles == "file" .. tostring(i) .. ".baxi" then
+            --if i == 1 and temporaryFiles == "file" .. tostring(i) .. ".baxi" then
                 table.insert(tableMissing, 1)
                 print("file1.baxi not found, creating...")
             --[[else
                 table.insert(tableMissing, i)
-                print("file found: looking for others...")]]
-            end
-            for k, v in ipairs(tableFilled) do
+                print("file found: looking for others...")
+            end]]
+            --[[for k, v in ipairs(tableFilled) do
                 if tableFilled[k] == tableMissing[#tableMissing] and tableFilled[k] <= tableMissing[#tableMissing] then
                     table.remove(tableMissing, k)
                     print("Duplicated index: deleting from table")
                 end
+            end]]
+        elseif string.match(temporaryFiles, "file" .. tostring(i) .. ".baxi$") and i ~= 1 then
+            table.insert(tableFilled, i)
+            table.insert(tableMissing, i)
+            for k, v in ipairs(tableFilled) do
+                if tableFilled[k] == tableMissing[#tableMissing] then
+                    table.remove(tableMissing, v)
+                    print("Duplicated index: deleting from table")
+                end
+            end
+            if #tableMissing == 0 then
+                table.remove(tableMissing, #tableMissing)
+            end
+            if #tableFilled == 0 then
+                table.remove(tableFilled, #tableFilled)
             end
         end
     end
+end
 --primeiro colocar tudo na tabela, depois organizar
         --[[
+            1, 2, 3, 4, 5
+            2, 7, 4, 9, 3   filled
+            1, 2            missing
+
+
             iniciando do menor para o maior elemento da lista
                 se a string combinar com o nome e estiver com prefixos (indice) e sufixos
                     se indice for 1 e valor for diferente de 1 entao
@@ -51,11 +72,10 @@ for i = 1, #list do
                             print("Duplicated index: deleting from table")
 
             1, 2, 3, 4, 5
-            2, 7, 4, 9, 3
-            1, 2
+            2, 7, 4, 9, 3   filled
+            1, 2            missing
 
         ]]
-end
 
 for files in list:inlines() do
     baxiLoopCount = 1 + baxiLoopCount
