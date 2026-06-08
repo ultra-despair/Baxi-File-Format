f7304fb509764a9a2d5f454075e052b7c993c3cc
ultra-despair <despair-ultra@proton.me>
2026-06-08 01:33:26 -0300

    pre-final refactoring

--- main.lua
+++ main.lua
@@ -1,8 +1,28 @@
 ---@diagnostic disable: trailing-space, lowercase-global, undefined-field, undefined-global
 
+--gerador de arquivos.baxi
+
 local baxiLoopCount = 0
 
 local list = io.popen("ls")
+
+--[[anotar todos os arquivos do dir filtrar apenas oa que quero, e se o tamanho do diretorio for maior 
+ou igual ao tamanho da lista padrao ai anote todos os arquivos]]
+
+local listFilesInDirectory = {}
+
+local sumAllBaxi = 0
+
+local index = 0
+
+for temporaryList in list:inlines() do
+    table.insert(listFilesInDirectory, index + 1, temporaryList)
+    for init = 1, #listFilesInDirectory do
+        if string.match(files, "file" .. tostring(init) .. ".baxi$") then
+            sumAllBaxi = sumAllBaxi + 1
+        end
+    end
+end
 
 local tableFilled = {
     0
@@ -12,23 +32,56 @@ local tableMissing = {
     0
 }
 
-for i = 1, #list do
+local higherFilled = 0
+
+--[[
+    1, 2, 3, 4, 5 indices
+    8, 2, 6, 3, 9 filled
+    1, 4 missing e se indice for ~= de valor  
+]]
+--[[
+    L1: 5
+]]
+for i = 1, sumAllBaxi do
+    if string.match(files, "file" .. tostring(i) .. ".baxi$") then
+        table.insert(tableFilled, i, i)
+    end
+    if higherFilled < tableFilled[i] then
+        higherFilled = tableFilled[i]
+    end
+end
+for index = 1, higherFilled do
+    if tableFilled[index] > index then
+        table.insert(tableMissing, index, index)
+        for k, v in ipairs(tableMissing) do
+            if tableFilled[index] == v then
+                table.remove(tableMissing, k)
+            end
+        end
+    elseif tableFilled[index] == nil then
+        table.insert(tableMissing, index, index)
+    end
+    if tableMissing[1] == 0 then
+        table.remove(tableMissing, tableMissing[1])
+    end
+end
+--[[for i = 1, #list do
     for temporaryFiles in list:inlines() do
         if string.match(temporaryFiles, "file" .. tostring(i) .. ".baxi$") and i == 1 then
             --table.insert(tableFilled, i)
             --if i == 1 and temporaryFiles == "file" .. tostring(i) .. ".baxi" then
                 table.insert(tableMissing, 1)
                 print("file1.baxi not found, creating...")
-            --[[else
+            else
                 table.insert(tableMissing, i)
                 print("file found: looking for others...")
-            end]]
-            --[[for k, v in ipairs(tableFilled) do
+            end
+            for k, v in ipairs(tableFilled) do
                 if tableFilled[k] == tableMissing[#tableMissing] and tableFilled[k] <= tableMissing[#tableMissing] then
                     table.remove(tableMissing, k)
                     print("Duplicated index: deleting from table")
                 end
-            end]]
+            end
         elseif string.match(temporaryFiles, "file" .. tostring(i) .. ".baxi$") and i ~= 1 then
             table.insert(tableFilled, i)
             table.insert(tableMissing, i)
@@ -46,7 +99,7 @@ for i = 1, #list do
             end
         end
     end
-end
+end]]
 --primeiro colocar tudo na tabela, depois organizar
         --[[
             1, 2, 3, 4, 5
@@ -76,14 +129,20 @@ end
             1, 2            missing
 
         ]]
+        
+--ve as linhas do list 
 
-for files in list:inlines() do
+--[[for files in list:inlines() do
     baxiLoopCount = 1 + baxiLoopCount
     if string.match(files, "file" .. tostring(baxiLoopCount) .. ".baxi$") then
         print("\"file " .. baxiLoopCount .. ".baxi\" already exists looking for next ...")
     else
         write = io.open("file" .. tostring(baxiLoopCount) .. ".baxi", "w")
     end
+end]]
+
+for i = 1, #tableMissing do
+    write = io.open("file" .. tostring(i) .. ".baxi", "w")
 end
 
 print("Bem vindo ao baxi file maker, aqui você pode escrever informações para serem convertidas em .baxi")
--- test.lua
+++ test.lua
@@ -12,8 +12,18 @@ if 1 and 2 < 3 then
     print("oloko")
 end]]
 
-tab = {}
+--[[tab = {}
 
 table.insert(tab, 1, 10)
 
-print(tab[1])
+print(tab[1])]]
+
+--[[tab = {}
+
+print(#tab)]]
+
+nulo = nil
+
+if 1 == nulo then
+    print("Hello")
+end
