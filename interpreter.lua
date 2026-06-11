8116fc46038ff7adc11575165e048739f24bbc8b
ultra-despair <despair-ultra@proton.me>
2026-06-11 20:07:45 -0300

    Functional interpreter/lib first version

--- interpreter.lua
+++ interpreter.lua
@@ -1,31 +1,19 @@
 ---@diagnostic disable: trailing-space, lowercase-global, undefined-field, undefined-global
 
---[[
-
-    ler em linhas 1
-    detectar quantas linhas sao dentro de outra 1
-    transformar em string/variavel/tabela 0
-
-    poder chamar com base no numero da linha
-
-]]
-
 local infoTable = {}
 
 local breakTable = {} --save the line of the break 
 
 local valuesTables = {} --saves the values by lines
 
-local loopUtils = {}
-
-local testBaxi = io.open("file1.baxi", "r")
+local loopUtils = {} --utils for loop temporary variables
 
 baxinterpreter = {
     readInLines = function (file)
-        --add fileOpen = io.open(file), "r")
+        fileOpen = io.open(file, "r")
         loopUtils.tableCount = 0 --for infoTable
         loopUtils.removeNils = 0
-        for interpreterLine in file:lines() do
+        for interpreterLine in fileOpen:lines() do
             loopUtils.tableCount = loopUtils.tableCount + 1
             table.insert(infoTable, loopUtils.tableCount, interpreterLine)
         end
@@ -38,20 +26,26 @@ baxinterpreter = {
             loopUtils.removeNils = loopUtils.removeNils + 1 
             if valuesTables[loopUtils.removeNils] == nil then
                 table.remove(valuesTables, loopUtils.removeNils)
+                table.insert(breakTable, #breakTable + 1, loopUtils.removeNils)
             end
         until loopUtils.removeNils == #valuesTables
-        --debug 
-        print("Infotable")
-        for i = 1, #infoTable do
-            print(infoTable[i])
-        end
-        print("\nValuestable")
-        for i = 1, #valuesTables do
-            --if valuesTables[i] ~= nil then
-                print(valuesTables[i])
-            --end
+    end,
+    callLine = function (lineFromBaxi, specifiedLine)
+        loopUtils.findLine = 0
+        if lineFromBaxi == 1 then
+            for start = 1, #breakTable do
+                if start ~= breakTable[1] then
+                    return valuesTables[specifiedLine]
+                end
+            end
+        else
+            loopUtils.findLine = lineFromBaxi + 1
+            for index = 1, #infoTable do
+                if loopUtils.findLine ~= breakTable[index + 1] then
+                    return valuesTables[specifiedLine + breakTable[index] - 1]
+                end
+                loopUtils.findLine = loopUtils.findLine + 1
+            end
         end
     end
 }
-
-baxinterpreter.readInLines(testBaxi)
