ab85fc4aaadf2024ff7acfb7e66a7a234e7cd549
ultra-despair <despair-ultra@proton.me>
2026-06-11 03:34:12 -0300

    Nil merged with valuesTables

--- interpreter.lua
+++ interpreter.lua
@@ -1,2 +1,48 @@
-print("WIP")
+---@diagnostic disable: trailing-space, lowercase-global, undefined-field, undefined-global
 
+--[[
+
+    ler em linhas 1
+    detectar quantas linhas sao dentro de outra 1
+    transformar em string/variavel/tabela 0
+
+    poder chamar com base no numero da linha
+
+]]
+
+local infoTable = {}
+
+local breakTable = {} --save the line of the break 
+
+local valuesTables = {} --saves the values by lines
+
+local loopUtils = {}
+
+local testBaxi = io.open("file1.baxi", "r")
+
+baxinterpreter = {
+    readInLines = function (file)
+        --add fileOpen = io.open(file), "r")
+        loopUtils.tableCount = 0 --for infoTable
+        for interpreterLine in file:lines() do
+            loopUtils.tableCount = loopUtils.tableCount + 1
+            table.insert(infoTable, loopUtils.tableCount, interpreterLine)
+        end
+        for start = 1, #infoTable do
+            if infoTable[start] ~= "" then
+                valuesTables[start] = infoTable[start]
+            end
+        end 
+        --debug 
+        print("Infotable")
+        for i = 1, #infoTable do
+            print(infoTable[i])
+        end
+        print("\nValuestable")
+        for i = 1, #valuesTables do
+            print(valuesTables[i])
+        end
+    end
+}
+
+baxinterpreter.readInLines(testBaxi)
